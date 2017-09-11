7104c718ae34fa4e42aefdf3b81a510e32eb69bc
cvasfi <yunus.ec@gmail.com>
2017-09-11 11:13:46 +0200

    depthwise separable resnet50

--- TinyImagenet/ResNet50_TI.py
+++ TinyImagenet/ResNet50_TI.py
@@ -51,6 +51,41 @@ class model(object):
           self.cost = tf.reduce_mean(xent, name='xent')
           self.cost += self._decay()
 
+    def build_dw(self,x,labels):
+        print("resnet dw build start: ")
+        print(x.get_shape())
+        input=tf.identity(x,"input")
+        with tf.variable_scope('init'):
+            x=self.conv_bn_relu(input,3,32,[1,1,1,1])   #128,56,56,32
+
+        with tf.variable_scope('stack1'):
+            for i in range(3):
+                x=self.block_dw(x,str(i),64,(i==0),False)   #128,56,56,16
+
+        with tf.variable_scope('stack2'):
+            for i in range(4):
+                x=self.block_dw(x,str(i),128,(i==0))   #128,28,28,16
+
+        with tf.variable_scope('stack3'):
+            for i in range(6):
+                x=self.block_dw(x,str(i),256,(i==0))   #128,14,14,16
+
+        with tf.variable_scope('stack4'):
+            for i in range(3):
+                x=self.block_dw(x,str(i),256,(i==0))   #128,7,7,16
+
+        x = tf.reduce_mean(x, reduction_indices=[1, 2], name="avg_pool")
+
+        with tf.variable_scope('fc'):
+            x=self.fc(x,200)
+
+        self.logits=x
+        self.predictions = tf.nn.softmax(self.logits,name="softmax")
+        with tf.variable_scope('costs'):
+          xent = tf.nn.softmax_cross_entropy_with_logits(
+              logits=self.logits, labels=labels)
+          self.cost = tf.reduce_mean(xent, name='xent')
+          self.cost += self._decay()
 
     def block(self,x,name,outs,project=False,downsample=True):
         shortcut=x
@@ -70,10 +105,49 @@ class model(object):
                     shortcut = self.bn(shortcut)
             return activation(x+shortcut)
 
+    def block_dw(self,x,name,outs,project=False,downsample=True):
+        shortcut=x
+        strides=[1,2,2,1] if (project and downsample) else [1,1,1,1]
+        with tf.variable_scope(name):
+            with tf.variable_scope("B1"):
+                x=self.conv_bn_relu(x,1,outs,strides)
+            with tf.variable_scope("B2"):
+                x=self.dw_conv_bn_relu(x,3,outs)
+            with tf.variable_scope("B3"):
+                x = self.conv(x, 1, outs*4)
+                x= self.bn(x)
+
+            with tf.variable_scope('shortcut'):
+                if project:
+                    shortcut = self.conv(shortcut, 1, outs*4,strides)
+                    shortcut = self.bn(shortcut)
+            return activation(x+shortcut)
+
     def conv_bn_relu(self,x,ksize,outs,strides=[1,1,1,1]):
         x=self.conv(x,ksize,outs,strides=strides)
         x=self.bn(x)
         return activation(x)
+
+    def dw_conv_bn_relu(self, x, ksize, outs, strides=[1, 1, 1, 1]):
+        x = self.dw_conv(x, ksize, outs, strides=strides)
+        x = self.bn(x)
+        return activation(x)
+
+    def dw_conv(self,x,ksize,outs,strides=[1,1,1,1]):
+        ins = x.get_shape()[-1]
+        n = ksize * ksize * 1
+        dw_outs = 1  # depth multiplier: 1 filter per channel
+        with tf.variable_scope("depthwise"):
+            dw_weights = tf.get_variable(
+                'DW', [ksize, ksize, ins, dw_outs],
+                tf.float32, initializer=tf.random_normal_initializer(
+                    stddev=np.sqrt(2.0 / n)))
+            dws = tf.nn.depthwise_conv2d(x, dw_weights, strides, padding='SAME')
+            dws=self.bn(dws)
+            dws = activation(dws)
+            #
+        with tf.variable_scope("pointwise"):
+            return self.conv(dws, 1, outs, strides=[1, 1, 1, 1])
 
     def conv(self,x,ksize,outs,strides=[1,1,1,1]):
         n = ksize * ksize * outs
