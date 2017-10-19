e181e6aac1aac3e499c5318143b3fffba54186e7
cvasfi <yunus.ec@gmail.com>
2017-10-19 21:11:52 +0200

    lookup eval

--- TinyImagenet/main.py
+++ TinyImagenet/main.py
@@ -6,6 +6,11 @@ import time
 import input_pipeline
 import AlexNet_TI
 import ResNet50_TI
+from LookupConvolution2d import extract_dense_weights
+from tensorflow.python.client import timeline
+import logging
+
+test_size=10000
 
 def train(batch_size,classes,FLAGS):
     print("train starts")
@@ -158,11 +163,14 @@ def eval(batch_size,classes,FLAGS):
     total_prediction, correct_prediction = 0, 0
     duration=0
 
-    for counter in six.moves.range(10000/batch_size):
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
+
+    for counter in six.moves.range(test_size/batch_size):
         start_time = time.time()
         (summaries, loss, predictions, truth, train_step) = sess.run(
             [model_summaries, model.cost, model.predictions,
-             labels, model.global_step])
+             labels, model.global_step], options=run_options, run_metadata=run_metadata)
         if(counter != 0):
             duration += time.time()-start_time
         # print("duration is: "+str(duration))
@@ -188,10 +196,176 @@ def eval(batch_size,classes,FLAGS):
                     (loss, precision, best_precision))
     summary_writer.flush()
 
-    print"average duration of a single image:" + str((duration/((10000/batch_size)-1))/batch_size)
-    print"duration of a single batch:" + str((duration/((10000/batch_size)-1)))
-    print"batches taken into account:" + str((((10000/batch_size)-1)))
+    tl = timeline.Timeline(run_metadata.step_stats)
+    ctf = tl.generate_chrome_trace_format()
+    with open(model_path+'_timeline.json', 'w') as f:
+        f.write(ctf)
+
+    print"average duration of a single image:" + str((duration/((test_size/batch_size)-1))/batch_size)
+    print"duration of a single batch:" + str((duration/((test_size/batch_size)-1)))
+    print"batches taken into account:" + str((((test_size/batch_size)-1)))
     print"total:" + str((duration))
+
+####################################################
+
+def lookup_eval(batch_size,classes,FLAGS):
+
+    print("lookup initialization starts")
+    filenames, all_labels = input_pipeline.read_labeled_image_list("validation_labels.csv")
+    images, labels = input_pipeline.load_batches(image_filenames=filenames,
+                                                 label_filenames=all_labels,
+                                                 network=FLAGS.network,
+                                                 shape=(64, 64, 3),
+                                                 batch_size=batch_size)
+    print("labels shale")
+    print(labels.get_shape)
+
+    if (FLAGS.network == "alexnet"):
+        model = AlexNet_TI.model(classes, "test", batch_size)
+        model_path = "alexNet_model"
+    elif (FLAGS.network == "resnet50"):
+        model = ResNet50_TI.model(classes, "test", batch_size)
+        model_path = "resnet50_model"
+
+    if (FLAGS.cnn == "dws"):
+        model.build_dw(images, labels)
+        model_path = model_path + "_dw"
+    elif (FLAGS.cnn == "lookup"):
+        model.build_lookup(images, labels)
+        model_path = model_path + "_l"
+    else:
+        model.build(images, labels)
+
+    saver = tf.train.Saver()
+    summary_writer = tf.summary.FileWriter(model_path + "/test")
+
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    tf.train.start_queue_runners(sess)
+    best_precision = 0.0
+
+    i = 0
+    total_duration = 0
+
+    try:
+        ckpt_state = tf.train.get_checkpoint_state(model_path)
+    except tf.errors.OutOfRangeError as e:
+        tf.logging.error('Cannot restore checkpoint: %s', e)
+    if not (ckpt_state and ckpt_state.model_checkpoint_path):
+        tf.logging.info('No model to eval yet at %s', model_path)
+    tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
+    saver.restore(sess, ckpt_state.model_checkpoint_path)
+
+    model_summaries = tf.summary.merge_all()
+    total_prediction, correct_prediction = 0, 0
+
+    (summaries, loss, predictions, truth, train_step) = sess.run(
+        [model_summaries, model.cost, model.predictions,
+         labels, model.global_step])
+    if (FLAGS.network == "alexnet"):
+        gr = tf.get_default_graph()
+        tensors = [gr.get_tensor_by_name('C%d/align_conv/kernel:0' % (convid + 1)) for convid in range(5)]
+        kernel_vals = sess.run(tensors)
+        for kernel_val in kernel_vals:
+            print kernel_val.size
+            print (np.count_nonzero(kernel_val) / kernel_val.size)
+
+    extract_dense_weights(sess)
+
+    tf.reset_default_graph()
+
+    ##############################################################
+
+    print("lookup eval starts")
+    filenames, all_labels = input_pipeline.read_labeled_image_list("validation_labels.csv")
+    images, labels = input_pipeline.load_batches(image_filenames=filenames,
+                 label_filenames=all_labels,
+                 network=FLAGS.network,
+                 shape=(64, 64, 3),
+                 batch_size=batch_size)
+    print("labels shale")
+    print(labels.get_shape)
+
+    if(FLAGS.network=="alexnet"):
+        model = AlexNet_TI.model(classes,"test",batch_size)
+        model_path="alexNet_model"
+    elif(FLAGS.network=="resnet50"):
+        model= ResNet50_TI.model(classes,"test",batch_size)
+        model_path="resnet50_model"
+
+    if(FLAGS.cnn=="dws"):
+        model.build_dw(images, labels)
+        model_path=model_path+"_dw"
+    elif (FLAGS.cnn == "lookup"):
+        model.build_lookup(images, labels)
+        model_path = model_path + "_l"
+    else:
+        model.build(images, labels)
+
+    saver = tf.train.Saver()
+    summary_writer = tf.summary.FileWriter(model_path+"/test")
+
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    tf.train.start_queue_runners(sess)
+    best_precision = 0.0
+
+    i=0
+    total_duration=0
+
+    try:
+        ckpt_state = tf.train.get_checkpoint_state(model_path)
+    except tf.errors.OutOfRangeError as e:
+        tf.logging.error('Cannot restore checkpoint: %s', e)
+    if not (ckpt_state and ckpt_state.model_checkpoint_path):
+        tf.logging.info('No model to eval yet at %s', model_path)
+    tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
+    saver.restore(sess, ckpt_state.model_checkpoint_path)
+
+    model_summaries = tf.summary.merge_all()
+    total_prediction, correct_prediction = 0, 0
+    duration=0
+
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
+
+    for counter in six.moves.range(test_size/batch_size):
+        start_time = time.time()
+        (summaries, loss, predictions, truth, train_step) = sess.run(
+            [model_summaries, model.cost, model.predictions,
+             labels, model.global_step], options=run_options, run_metadata=run_metadata)
+        if(counter != 0):
+            duration += time.time()-start_time
+        print("duration is: "+str(duration))
+        print(duration)
+        truth = np.argmax(truth, axis=1)
+        predictions = np.argmax(predictions, axis=1)
+        correct_prediction += np.sum(truth == predictions)
+        total_prediction += predictions.shape[0]
+
+    precision = 1.0 * correct_prediction / total_prediction
+    best_precision = max(precision, best_precision)
+
+    precision_summ = tf.Summary()
+    precision_summ.value.add(
+        tag='Precision', simple_value=precision)
+    summary_writer.add_summary(precision_summ, train_step)
+    best_precision_summ = tf.Summary()
+    best_precision_summ.value.add(
+        tag='Best Precision', simple_value=best_precision)
+    summary_writer.add_summary(best_precision_summ, train_step)
+    summary_writer.add_summary(summaries, train_step)
+    tf.logging.info('loss: %.3f, precision: %.3f, best precision: %.3f' %
+                    (loss, precision, best_precision))
+    summary_writer.flush()
+
+
+    print"average duration of a single image:" + str((duration/((test_size/batch_size)-1))/batch_size)
+    print"duration of a single batch:" + str((duration/((test_size/batch_size)-1)))
+    print"batches taken into account:" + str((((test_size/batch_size)-1)))
+    print"total:" + str((duration))
+    tl = timeline.Timeline(run_metadata.step_stats)
+    ctf = tl.generate_chrome_trace_format()
+    with open(model_path+'_timeline.json', 'w') as f:
+        f.write(ctf)
 
 
 FLAGS = tf.app.flags.FLAGS
@@ -207,7 +381,10 @@ def main(_):
         if FLAGS.mode=="train":
             train(50,200,FLAGS)
         else:
-            eval(50,200,FLAGS)
+            if (FLAGS.cnn=="lookup"):
+                lookup_eval(1,200,FLAGS)
+            else:
+                eval(1,200,FLAGS)
 
 
 if __name__ == '__main__':
